104abc3e3b75ed6fbe7aeefac52e3987efe6b44b
Pawan Chaurasia <logan@MacBook-Pro.local>
2020-02-21 09:30:13 +0530

    find any peak element in any unsorted array

---
+++ searching/find_all_peak_ele_in_arr.py
@@ -0,0 +1,27 @@
+#
+#  TODO need to do the TODO
+# def find_all_peak_in_unsorted_arr(arr):
+#     lo = 0
+#     hi = len(arr) - 1
+#     res = -1
+#     max_hi = len(arr) - 1
+#     while lo <= hi:
+#         mid = (lo + hi) // 2
+#         if mid == 0 and arr[mid] >= arr[mid+1]:
+#             return arr[mid]
+#         if mid == max_hi and arr[mid] >= arr[mid-1]:
+#             return arr[mid]
+#         if mid != 0:
+#             if arr[mid] >= arr[mid+1] and arr[mid] >= arr[mid-1]:
+#                 return arr[mid]
+#             if arr[mid] >= arr[mid+1]:
+#                 hi = mid - 1
+#             else:
+#                 lo = mid + 1
+#     return res
+#
+#
+# arr = [1, 10, 10]
+# result = find_all_peak_in_unsorted_arr(arr)
+# print(result)
+#
