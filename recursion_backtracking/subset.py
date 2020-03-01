58a0b45378ff4a4d8f7d83a551fd9fd988d79d16
Pawan Chaurasia <logan@MacBook-Pro.local>
2020-03-01 20:26:21 +0530

    subset refactoring

--- recursion_backtracking/subset.py
+++ recursion_backtracking/subset.py
@@ -1,16 +1,14 @@
-# def find_subset(arr):
-#     sublist = [[]]
-#     for i in range(len(arr)+1):
-#         for j in range(i+1, len(arr)+1):
-#             print(i, j)
-#             sub = arr[i:j]
-#             print(sub)
-#             sublist.append(sub)
-#     return sublist
-#
-# arr = [1, 2, 3]
-# results = find_subset(arr)
-# print(results)
+def find_subset(arr):
+    sublist = [[]]
+    for i in range(len(arr)+1):
+        for j in range(i+1, len(arr)+1):
+            sub = arr[i:j]
+            sublist.append(sub)
+    return sublist
+
+arr = [1, 2, 3]
+results = find_subset(arr)
+print(results)
 
 
 # now by recursion
@@ -33,15 +31,31 @@
 # arr = [1, 2, 3]
 # subset_by_recursion(arr)
 
+#
+# def solution_recur(given_array, subset, index, len):
+#     print(subset)
+#     for i in range(index, len):
+#         subset.append(given_array[i])
+#         solution_recur(given_array,subset, i+1, len)
+#         subset.pop()
+#
+#
+# arr = [1, 2, 3]
+# solution_recur(arr, [], 0, len(arr))
 
-def solution_recur(given_array, index, len):
-    if index >= len:
-        print(given_array)
-        return
-    for i in range(len):
-        i
-        solution_recur(given_array[index:i], index+1, len)
 
-
-arr = [1, 2]
-solution_recur(arr, 0, len(arr))
+# def subset_lopper(arr, subset, index, results):
+#     results.append(subset.copy())
+#     for i in range(index, len(arr)):
+#         subset.append(arr[i])
+#         subset_lopper(arr, subset, i+1, results)
+#         subset.pop(-1)
+#     return
+#
+#
+# arr = [1, 2, 3]
+# subs = []
+# result = []
+# subset_lopper(arr, subs, 0, result)
+# print(result)
+#
